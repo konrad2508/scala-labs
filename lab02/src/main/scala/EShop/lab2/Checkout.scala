2c98688657398c204d13ed6c2e26ae4c6c921abd
Konrad G <konrad1997@gmail.com>
2019-11-04 22:57:09 +0100

    Implement Checkout

--- lab02/src/main/scala/EShop/lab2/Checkout.scala
+++ lab02/src/main/scala/EShop/lab2/Checkout.scala
@@ -1,7 +1,8 @@
 package EShop.lab2
 
+import EShop.lab2.Checkout._
 import akka.actor.{Actor, ActorRef, Cancellable, Props}
-import akka.event.Logging
+import akka.event.{Logging, LoggingReceive}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -23,8 +24,12 @@ object Checkout {
   case object ReceivePayment                      extends Command
 
   sealed trait Event
+  case object CheckoutStarted                  extends Event
   case object CheckOutClosed                   extends Event
   case class PaymentStarted(payment: ActorRef) extends Event
+  case object SelectedDelivery                 extends Event
+  case object SelectedPayment                  extends Event
+  case object ReceivedPayment                  extends Event
 
   def props(cart: ActorRef) = Props(new Checkout())
 }
@@ -37,16 +42,48 @@ class Checkout extends Actor {
   val checkoutTimerDuration = 1 seconds
   val paymentTimerDuration  = 1 seconds
 
-  def receive: Receive = ???
+  def receive: Receive = LoggingReceive {
+    case StartCheckout =>
+      println("Checkout started")
 
-  def selectingDelivery(timer: Cancellable): Receive = ???
+      context become selectingDelivery(null)
 
-  def selectingPaymentMethod(timer: Cancellable): Receive = ???
+      sender ! CheckoutStarted
+  }
 
-  def processingPayment(timer: Cancellable): Receive = ???
+  def selectingDelivery(timer: Cancellable): Receive = LoggingReceive {
+    case e: SelectDeliveryMethod =>
+      println("Selected delivery: " + e.method)
 
-  def cancelled: Receive = ???
+      context become selectingPaymentMethod(null)
 
-  def closed: Receive = ???
+      sender ! SelectedDelivery
+  }
+
+  def selectingPaymentMethod(timer: Cancellable): Receive = LoggingReceive {
+    case e: SelectPayment =>
+      println("Selected payment: " + e.payment)
+
+      context become processingPayment(null)
+
+      sender ! SelectedPayment
+  }
+
+  def processingPayment(timer: Cancellable): Receive = LoggingReceive {
+    case ReceivePayment =>
+      println("Payment received")
+
+      context become closed
+
+      sender ! ReceivedPayment
+  }
+
+  def cancelled: Receive = LoggingReceive {
+    case _ =>
+  }
+
+  def closed: Receive = LoggingReceive {
+    case _ =>
+  }
 
 }
