71ed319c956ad62c7b1005478ccd30bd6267d3d9
Konrad G <konrad1997@gmail.com>
2019-11-04 03:58:23 +0100

    Implement CartActor

--- lab02/src/main/scala/EShop/lab2/Cart.scala
+++ lab02/src/main/scala/EShop/lab2/Cart.scala
@@ -4,7 +4,7 @@ case class Cart(items: Seq[Any]) {
   def contains(item: Any): Boolean = items contains item
   def addItem(item: Any): Cart     = Cart(items :+ item)
   def removeItem(item: Any): Cart  = Cart(items diff Seq(item))
-  def size: Int                    = items size
+  def size: Int                    = items.size
 }
 
 object Cart {
--- lab02/src/main/scala/EShop/lab2/CartActor.scala
+++ lab02/src/main/scala/EShop/lab2/CartActor.scala
@@ -1,6 +1,6 @@
 package EShop.lab2
 
-import EShop.lab2.CartActor.{ItemAdded, ItemRemoved}
+import EShop.lab2.CartActor._
 import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
@@ -17,11 +17,12 @@ object CartActor {
   case object CloseCheckout        extends Command
 
   sealed trait Event
-  case class CheckoutStarted(checkoutRef: ActorRef) extends Event
+  case class CheckoutStarted(checkoutRef: ActorRef)   extends Event
   case class CheckoutCancelled(checkoutRef: ActorRef) extends Event
-  case class CheckoutClosed(checkoutRef: ActorRef) extends Event
-  case object ItemAdded extends Event
-  case object ItemRemoved extends Event
+  case class CheckoutClosed(checkoutRef: ActorRef)    extends Event
+  case object ItemAdded                               extends Event
+  case object ItemRemoved                             extends Event
+  case object CartExpired                             extends Event
 
   def props = Props(new CartActor())
 }
@@ -35,40 +36,74 @@ class CartActor extends Actor {
   def receive: Receive = empty
 
   def empty: Receive = LoggingReceive {
-    case ItemAdded =>
-      val cart = new Cart(null)
-      cart.addItem()
-      println("Adding sth (empty)")
+    case e: AddItem =>
+      var cart = Cart.empty
+      cart = cart addItem e.item
+      println("Adding " + e.item)
+
       context become nonEmpty(cart, null)
-    case _ => ???
+
+      sender ! ItemAdded
   }
 
   def nonEmpty(cart: Cart, timer: Cancellable): Receive = LoggingReceive {
-    case ItemAdded =>
-      cart.addItem()
-      println("Adding sth (nonEmpty)")
-    case ItemRemoved if cart.size != 1 =>
-      cart.removeItem()
-      println("Removing sth (nonEmpty)")
-    case ItemRemoved if cart.size == 1 =>
-      cart.removeItem()
-      println("Removing sth (nowEmpty)")
+    case e: AddItem =>
+      val newCart = cart addItem e.item
+      println("Adding " + e.item)
+
+      context become nonEmpty(newCart, null)
+
+      sender ! ItemAdded
+
+    case e: RemoveItem if cart.size == 1 =>
+      cart removeItem e.item
+      println("Removing " + e.item + " (last item)")
+
       context become empty
-    case _ => ???
+
+      sender ! ItemRemoved
+
+    case e: RemoveItem =>
+      val newCart = cart removeItem e.item
+      println("Removing " + e.item)
+
+      context become nonEmpty(newCart, null)
+
+      sender ! ItemRemoved
+
+    case ExpireCart =>
+      println("Cart expired")
+
+      context become empty
+
+      sender ! CartExpired
+
+    case StartCheckout =>
+      println("Checkout started")
+
+      context become inCheckout(cart)
+
+      sender ! CheckoutStarted
   }
 
-  def inCheckout(cart: Cart): Receive = ???
+  def inCheckout(cart: Cart): Receive = LoggingReceive {
+    case CancelCheckout =>
+      println("Checkout cancelled")
 
+      context become nonEmpty(cart, null)
+
+      sender ! CheckoutCancelled
+
+    case CloseCheckout =>
+      println("Checkout closed")
+
+      context become empty
+
+      sender ! CheckoutClosed
+  }
 }
 
 object MainApp extends App {
   val system = ActorSystem("TestoSystem")
-  val cart = system.actorOf(Props[CartActor], name = "cart1")
-
-  cart ! ItemAdded
-  cart ! ItemAdded
-  cart ! ItemAdded
-  cart ! ItemRemoved
-  cart ! ItemRemoved
-  cart ! ItemRemoved
+  val cart   = system.actorOf(Props[CartActor], name = "cart1")
 }
