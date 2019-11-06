c364ac96175e6dc713403f6d806fd3a82ccccbf0
Konrad Grela <konrad1997@gmail.com>
2019-11-06 21:04:12 +0100

    Implement CartFSM

--- lab02/src/main/scala/EShop/lab2/CartActor.scala
+++ lab02/src/main/scala/EShop/lab2/CartActor.scala
@@ -1,7 +1,7 @@
 package EShop.lab2
 
 import EShop.lab2.CartActor._
-import akka.actor.{Actor, ActorRef, ActorSystem, Cancellable, Props, Timers}
+import akka.actor.{Actor, ActorRef, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
 import scala.concurrent.duration._
@@ -42,7 +42,6 @@ class CartActor extends Actor {
       cart = cart addItem e.item
 
 //      sender ! ItemAdded
-
       context become nonEmpty(cart, scheduleTimer)
   }
 
@@ -53,7 +52,6 @@ class CartActor extends Actor {
       val newCart = cart addItem e.item
 
 //      sender ! ItemAdded
-
       context become nonEmpty(newCart, scheduleTimer)
 
     case e: RemoveItem if (cart contains e.item) && cart.size == 1 =>
@@ -62,7 +60,6 @@ class CartActor extends Actor {
       cart removeItem e.item
 
 //      sender ! ItemRemoved
-
       context become empty
 
     case e: RemoveItem if cart contains e.item =>
@@ -71,32 +68,27 @@ class CartActor extends Actor {
       val newCart = cart removeItem e.item
 
 //      sender ! ItemRemoved
-
       context become nonEmpty(newCart, scheduleTimer)
 
     case ExpireCart =>
       timer.cancel()
 
 //      sender ! CartExpired
-
       context become empty
 
     case StartCheckout =>
       timer.cancel()
 //      sender ! CheckoutStarted
-
       context become inCheckout(cart)
   }
 
   def inCheckout(cart: Cart): Receive = LoggingReceive {
     case CancelCheckout =>
 //      sender ! CheckoutCancelled
-
       context become nonEmpty(cart, scheduleTimer)
 
     case CloseCheckout =>
 //      sender ! CheckoutClosed
-
       context become empty
   }
 }
--- lab02/src/main/scala/EShop/lab2/CartFSM.scala
+++ lab02/src/main/scala/EShop/lab2/CartFSM.scala
@@ -1,5 +1,6 @@
 package EShop.lab2
 
+import EShop.lab2.CartActor.{AddItem, CancelCheckout, CloseCheckout, RemoveItem, StartCheckout}
 import EShop.lab2.CartFSM.Status
 import akka.actor.{LoggingFSM, Props}
 
@@ -27,15 +28,27 @@ class CartFSM extends LoggingFSM[Status.Value, Cart] {
   startWith(Empty, Cart.empty)
 
   when(Empty) {
-    ???
+    case Event(e: AddItem, c: Cart) =>
+      goto(NonEmpty).using(c.addItem(e.item))
   }
 
   when(NonEmpty, stateTimeout = cartTimerDuration) {
-    ???
+    case Event(e: AddItem, c: Cart) =>
+      stay().using(c.addItem(e.item))
+    case Event(e: RemoveItem, c: Cart) if c.contains(e.item) && c.size == 1 =>
+      goto(Empty).using(c.removeItem(e.item))
+    case Event(e: RemoveItem, c: Cart) if c.contains(e.item) =>
+      stay().using(c.removeItem(e.item))
+    case Event(StartCheckout, c: Cart) =>
+      goto(InCheckout).using(c)
+    case Event(StateTimeout, _) =>
+      goto(Empty).using(Cart.empty)
   }
 
   when(InCheckout) {
-    ???
+    case Event(CancelCheckout, c: Cart) =>
+      goto(NonEmpty).using(c)
+    case Event(CloseCheckout, c: Cart) =>
+      goto(Empty).using(Cart.empty)
   }
-
 }
