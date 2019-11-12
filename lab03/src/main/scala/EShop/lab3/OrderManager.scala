7b3d13412fdcc80ecd330eb69f31f5967c466898
Konrad Grela <konrad1997@gmail.com>
2019-11-13 00:54:10 +0100

    Accommodate to OrderManager solution template

--- lab03/src/main/scala/EShop/lab3/OrderManager.scala
+++ lab03/src/main/scala/EShop/lab3/OrderManager.scala
@@ -2,49 +2,77 @@ package EShop.lab3
 
 import EShop.lab2.{CartActor, Checkout}
 import EShop.lab3.OrderManager._
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{Actor, ActorRef}
+import akka.event.LoggingReceive
 
 object OrderManager {
-  sealed trait Command
-  case class AddItem(item: Any) extends Command
-  case class RemoveItem(item: Any) extends Command
-  case class SelectDeliveryMethod(method: String) extends Command
-  case class SelectPaymentMethod(method: String) extends Command
-  case object StartCheckout extends Command
-  case object DoPayment extends Command
+  sealed trait State
+  case object Uninitialized extends State
+  case object Open          extends State
+  case object InCheckout    extends State
+  case object InPayment     extends State
+  case object Finished      extends State
 
-  def props = Props(new OrderManager())
+  sealed trait Command
+  case class AddItem(id: String)                                               extends Command
+  case class RemoveItem(id: String)                                            extends Command
+  case class SelectDeliveryAndPaymentMethod(delivery: String, payment: String) extends Command
+  case object Buy                                                              extends Command
+  case object Pay                                                              extends Command
+
+  sealed trait Ack
+  case object Done extends Ack //trivial ACK
+
+  sealed trait Data
+  case object Empty                                                            extends Data
+  case class CartData(cartRef: ActorRef)                                       extends Data
+  case class CartDataWithSender(cartRef: ActorRef, sender: ActorRef)           extends Data
+  case class InCheckoutData(checkoutRef: ActorRef)                             extends Data
+  case class InCheckoutDataWithSender(checkoutRef: ActorRef, sender: ActorRef) extends Data
+  case class InPaymentData(paymentRef: ActorRef)                               extends Data
+  case class InPaymentDataWithSender(paymentRef: ActorRef, sender: ActorRef)   extends Data
 }
 
 class OrderManager extends Actor {
-  override def receive: Receive = {
+
+  override def receive: Receive = uninitialized
+
+  def uninitialized: Receive = LoggingReceive {
     val cartRef = context.system.actorOf(CartActor.props(self))
-    ordering(cartRef)
+    open(cartRef)
   }
 
-  def ordering(cartRef: ActorRef): Receive = {
-    case e: AddItem => cartRef ! CartActor.AddItem(e.item)
-    case e: RemoveItem => cartRef ! CartActor.RemoveItem(e.item)
-    case StartCheckout => cartRef ! CartActor.StartCheckout
-    case e: CartActor.CheckoutStarted => context become checkingOut(e.checkoutRef)
+  def open(cartActor: ActorRef): Receive = LoggingReceive {
+    case e: AddItem => cartActor ! CartActor.AddItem(e.id)
+    case e: RemoveItem => cartActor ! CartActor.RemoveItem(e.id)
+    case Buy =>
+      cartActor ! CartActor.StartCheckout
+      context become inCheckout(cartActor, sender)
   }
 
-  def checkingOut(checkoutRef: ActorRef): Receive = {
-    case e: SelectDeliveryMethod => checkoutRef ! Checkout.SelectDeliveryMethod(e.method)
-    case e: SelectPaymentMethod => checkoutRef ! Checkout.SelectPayment(e.method)
-    case e: Checkout.PaymentStarted => context become payment(e.paymentRef)
+  def inCheckout(cartActorRef: ActorRef, senderRef: ActorRef): Receive = LoggingReceive {
+    case e: CartActor.CheckoutStarted => context become inCheckout(e.checkoutRef, senderRef)
   }
 
-  def payment(paymentRef: ActorRef): Receive = {
-    case DoPayment => paymentRef ! Payment.Pay
-    case Payment.PaymentFinished => context become closeCheckout
+  def inCheckout(checkoutActorRef: ActorRef, senderRef: ActorRef): Receive = LoggingReceive {
+    case e: SelectDeliveryAndPaymentMethod =>
+      checkoutActorRef ! Checkout.SelectDeliveryMethod(e.delivery)
+      checkoutActorRef ! Checkout.SelectPayment(e.payment)
+
+      context become inPayment(senderRef)
   }
 
-  def closeCheckout: Receive = {
-    case Checkout.CheckoutClosed => context become closeCart
+  def inPayment(senderRef: ActorRef): Receive = LoggingReceive {
+    case Checkout.PaymentStarted(paymentRef) => context become inPayment(paymentRef, senderRef)
+
   }
 
-  def closeCart: Receive = {
-    case CartActor.CartEmpty => context become ordering(sender)
+  def inPayment(paymentActorRef: ActorRef, senderRef: ActorRef): Receive = LoggingReceive {
+    case Pay                      => paymentActorRef ! Payment.Pay
+    case Payment.PaymentConfirmed => context become finished
+  }
+
+  def finished: Receive = LoggingReceive {
+    case _ => sender ! "order manager finished job"
   }
 }
